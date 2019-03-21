00025e96eb1dc20cdce849d836b84082362447a5
RyanFizz <44347981+RyanFizz@users.noreply.github.com>
2019-03-21 17:42:13 -0400

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const botconfig = require("../config.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = botconfig.prefix;
+    if (!message.content.startsWith(prefix)) return;
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+        .setThumbnail(bicon)
+        .addField("Default Prefix", `.`)
+        .addField("Moderation", `**purge:** Deletes any amount of messages 1-100.\n**ban:** Bans a user that you mention.\n**kick:** Kicks a user that you mention.\n**warn:** Warns a user that you mention.`)
+        .addBlankField()
+        .addField("Fun", `**gay:** Tells you how gay you are.\n**hug:** Hugs someone.\n**kiss:** Kisses someone.\n**cuddle:** Cuddles someone.`)
+        .addBlankField()
+        .addField("General", `**userinfo:** Shows info about a user.\n**serverinfo:** Shows info about the server\n**help:** Shows this message.`)
+        .addField("**Support Server:**", `https://discord.gg/jSfk822"`)
+        .setFooter(`Requested by : ${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL)
+        .setColor(0xff4242);
+
+    message.channel.send(botembed);
+}
+
+module.exports.help = {
+    name: "help"
+}
